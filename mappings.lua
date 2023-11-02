b071a3da4e2326320f7e33d1d8816f0be1925329
Igor <igor.gorohovsky@sembly.ai>
2023-11-02 10:14:21 +0200

    init

--- mappings.lua
+++ mappings.lua
@@ -4,37 +4,41 @@
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
 return {
-  -- first key is the mode
-  n = {
-    -- second key is the lefthand side of the map
+    -- first key is the mode
+    n = {
+        -- second key is the lefthand side of the map
 
-    -- navigate buffer tabs with `H` and `L`
-    -- L = {
-    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-    --   desc = "Next buffer",
-    -- },
-    -- H = {
-    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-    --   desc = "Previous buffer",
-    -- },
+        -- navigate buffer tabs with `H` and `L`
+        -- L = {
+        --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+        --   desc = "Next buffer",
+        -- },
+        -- H = {
+        --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+        --   desc = "Previous buffer",
+        -- },
 
-    -- mappings seen under group name "Buffer"
-    ["<leader>bD"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(
-          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
-        )
-      end,
-      desc = "Pick to close",
+        -- mappings seen under group name "Buffer"
+        ["<leader>bD"] = {
+            function()
+                require("astronvim.utils.status").heirline.buffer_picker(
+                    function(bufnr)
+                        require("astronvim.utils.buffer").close(bufnr)
+                    end)
+            end,
+            desc = "Pick to close"
+        },
+        -- tables with the `name` key will be registered with which-key if it's installed
+        -- this is useful for naming menus
+        ["<leader>b"] = {name = "Buffers"},
+        ["d"] = {'"_d'},
+        ["c"] = {'"_c'}
+        -- quick save
+        -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
     },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-  },
-  t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
-  },
+    t = {
+        -- setting a mapping to false will disable it
+        -- ["<esc>"] = false,
+    },
+    v = {["d"] = {'"_d'}, ["c"] = {'"_c'}}
 }
--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,17 @@
 return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of importing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+    -- Add the community repository of plugin specifications
+    "AstroNvim/astrocommunity",
+    -- example of importing a plugin, comment out to use it or add your own
+    -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+    {import = "astrocommunity.colorscheme.catppuccin"},
+    {import = "astrocommunity.pack.python"},
+    {import = "astrocommunity.pack.toml"},
+    {import = "astrocommunity.pack.yaml"},
+    {import = "astrocommunity.pack.typescript"},
+    {import = "astrocommunity.completion.copilot-lua"},
+    {import = "astrocommunity.test.neotest"},
+    {import = "astrocommunity.motion.nvim-surround"},
+    {import = "astrocommunity.editing-support.multicursors-nvim"},
+    {"catppuccin/nvim", name = "catppuccin", opts = {term_colors = true}}
 }
